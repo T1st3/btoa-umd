bef45ce67baa287ec56e77cfbb89081a7e88b579
t1st3 <contact@tiste.org>
2014-07-24 05:08:46 +0200

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -517,13 +517,12 @@ gulp.task('coverage_browser_global', ['coverage_instrument'], function (cb) {
   });
 });
 
-gulp.task('coverage_browser_amd', ['coverage_instrument'], function (cb) {
+gulp.task('coverage_browser_amd', ['coverage_browser_global'], function (cb) {
   var cmd = './node_modules/mocha-phantomjs/bin/mocha-phantomjs ./test/tests_amd.html';
   cmd += ' -R json-cov -f ./tmp2/tmp.json';
   exec(cmd, function (err, stdout, stderr) {
     //console.log(stdout);
     console.log(stderr);
-    fs.mkdirParent('./tmp/');
     fs.writeFile('./tmp/coverage_amd.json', stdout, function(err) {
       if (err) {
         console.log(err);
